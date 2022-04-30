a5914c950a7a4ebc899489d205768072c7fe6461
Ronald chen <rc9525@gmail.com>
2022-04-30 16:21:34 -0400

    code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,14 +1,16 @@
 import { openDB } from "idb";
 
+
+
 const initdb = async () =>
   openDB("jate", 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains("jate")) {
-        console.log("jate database already exists");
+        
         return;
       }
       db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
-      console.log("jate database created");
+     
     },
   });
 
